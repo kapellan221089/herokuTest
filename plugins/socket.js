019e723b80d3463e40a1f710668cbfcd2cef2260
kapellan221089 <serg221089@gmail.com>
2022-08-27 22:42:56 +0300

    4

---
+++ plugins/socket.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import VueSocketIO from 'vue-socket.io';
+import SocketIO from 'socket.io-client';
+
+export default function({ store }) {
+  Vue.use(
+    new VueSocketIO({
+      debug: true,
+      connection: '',
+      vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+      }
+    })
+  )
+}
