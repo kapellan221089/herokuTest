7cf88f566e99fcb88f5662def5a58015184951ec
kapellan221089 <serg221089@gmail.com>
2022-08-28 12:11:09 +0300

    test

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import VueSocketIO from 'vue-socket.io';
 import SocketIO from 'socket.io-client';
+const port = process.env.PORT
 
 export default function({ store }) {
   Vue.use(
     new VueSocketIO({
-      debug: true,
-      connection: 'https://lit-taiga-04192.herokuapp.com:8080',
+      debug: false,
+      connection: SocketIO('https://mighty-taiga-11038.herokuapp.com:'+ port),
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
